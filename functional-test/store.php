22948a77dc5dd3d56453a28c75aea682a39629e6
Pierre <pierre.miniggio@gmail.com>
2020-01-10 08:19:42 +0100

    test json return error

--- functional-test/store.php
+++ functional-test/store.php
@@ -1,6 +1,12 @@
 <?php
 
 $file = $_FILES['file'];
+
+if (! empty($file['error'])) {
+	echo json_encode(['error' => $file['error']]);
+	die();
+}
+
 rename($file['tmp_name'], __DIR__ . '/files/' . $file['name']);
 
 echo json_encode(['success' => 1, 'file' => $file['name']]);
