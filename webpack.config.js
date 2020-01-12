6c82bb4fe1aa568bf504d3a26abbdff6b26ca47d
Pierre <pierre.miniggio@gmail.com>
2020-01-12 20:31:23 +0100

    clean sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,35 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const MinifyPlugin = require('babel-minify-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: {
-    'main': './src/js/main.js',
-    'OrderableFileManagerInstance': './src/js/OrderableFileManagerInstance.js',
-    'demo': './src/js/demo.js',
-    'OrderableFileManager': './src/sass/main.scss'
+    'main.js': './src/js/main.js',
+    'OrderableFileManagerInstance.js': './src/js/OrderableFileManagerInstance.js',
+    'demo.js': './src/js/demo.js'
   },
   output: {
-    filename: '[name].js',
+    filename: '[name]',
     path: path.resolve(__dirname, 'public/js'),
     library: "OrderableFileManager",
     libraryExport: 'default',
     libraryTarget: "umd"
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       }
-    },{
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: "style-loader",
-        use: ['css-loader','sass-loader']
-      })
-    }]
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(['public/css']),
-    new ExtractTextPlugin({
-      filename:  (getPath) => {
-        return getPath('[name].css');
-      }
-    }),
     new MinifyPlugin({}, {
       test: /\.min\.js$/,
     }),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.min\.css$/,
-      cssProcessorOptions: { discardComments: { removeAll: true } }
-    })
   ]
 };
