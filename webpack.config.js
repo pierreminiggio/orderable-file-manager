d529a5c290c5d2dbde2e6cb53ff6e178fe44dd47
Pierre <pierre.miniggio@gmail.com>
2020-01-12 20:59:23 +0100

    add sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const MinifyPlugin = require('babel-minify-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
   entry: {
-    'main.js': './src/js/main.js',
-    'OrderableFileManagerInstance.js': './src/js/OrderableFileManagerInstance.js',
-    'demo.js': './src/js/demo.js'
+    'main': './src/js/main.js',
+    'OrderableFileManagerInstance': './src/js/OrderableFileManagerInstance.js',
+    'demo': './src/js/demo.js',
+    'styles': './src/js/styles.js'
   },
   output: {
-    filename: '[name]',
-    path: path.resolve(__dirname, 'public/js'),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
     library: "OrderableFileManager",
     libraryExport: 'default',
     libraryTarget: "umd"
@@ -23,11 +26,51 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
       }
     ]
   },
+  resolve: {
+      extensions: ['.js', '.scss']
+  },
   plugins: [
-    new CleanWebpackPlugin(['public/css']),
+    new CleanWebpackPlugin(['public']),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
     new MinifyPlugin({}, {
       test: /\.min\.js$/,
     }),
